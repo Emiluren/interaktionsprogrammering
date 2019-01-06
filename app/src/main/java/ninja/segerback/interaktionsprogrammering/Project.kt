101b7302b205f4bcffc83136a1ece03bbe143d3c
Emil Segerbäck <emilsegerback@gmail.com>
2019-01-06 17:59:52 +0100

    Improve StepsLeft component

--- app/src/main/java/ninja/segerback/interaktionsprogrammering/Project.kt
+++ app/src/main/java/ninja/segerback/interaktionsprogrammering/Project.kt
@@ -1,9 +1,19 @@
 package ninja.segerback.interaktionsprogrammering
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.support.v4.view.ViewPager
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.LinearLayout
+import android.widget.TextView
 
 class Project : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +21,114 @@ class Project : FragmentActivity() {
 
         setContentView(R.layout.fragment_pager)
 
-        val adapter = StepsAdapter(supportFragmentManager)
+        val stepNames = listOf("Welcome", "Password", "End")
+        val steps = listOf({HelloFragment()}, {PasswordFragment()}, {EndFragment()})
+
+        val adapter = StepsAdapter(supportFragmentManager, steps)
         val pager: ViewPager = findViewById(R.id.pager)
         pager.adapter = adapter
 
+        val stepsLeft = StepsLeft(this, stepNames, pager)
+
+        pager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
+            override fun onPageSelected(position: Int) {
+                stepsLeft.highlightStep(position)
+            }
+        })
+
         val pageLayout: LinearLayout = findViewById(R.id.page)
-        pageLayout.addView(StepsLeft(this, listOf("Welcome", "Password", "End"), pager))
+        pageLayout.addView(stepsLeft)
+    }
+}
+
+class HelloFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_container, container, false)
+        val frame = v.findViewById<FrameLayout>(R.id.frame)
+
+        val textView = TextView(context)
+        textView.text = "Hello and welcome"
+        textView.textSize = 30.0f
+        frame.addView(textView)
+
+        return v
+    }
+}
+
+class PasswordFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_container, container, false)
+        val frame = v.findViewById<FrameLayout>(R.id.frame)
+
+        val c = context
+        if (c != null) {
+            val passwordStrengthMeter = PasswordStrengthMeter(c) { password ->
+                var score = 0
+
+                // Check password length
+                if (password.length > 5) {
+                    score++
+                }
+                if (password.length > 8) {
+                    score++
+                }
+
+                // Check that the password contains both upper and lower case letters
+                if (password.toLowerCase() != password && password.toUpperCase() != password) {
+                    score++
+                }
+
+                // Check that the password contains special characters
+                if (password.any { c -> !c.isLetter() }) {
+                    score++
+                }
+
+                when (score) {
+                    0 -> PasswordStrength.VeryWeak
+                    1 -> PasswordStrength.Weak
+                    2 -> PasswordStrength.Mediocre
+                    3 -> PasswordStrength.Good
+                    else -> PasswordStrength.Strong
+                }
+            }
+            passwordStrengthMeter.setVisualizer(TextPasswordVisualizer(c))
+
+            val passwordBox = EditText(context)
+            passwordBox.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            passwordBox.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable) {
+                    passwordStrengthMeter.showStrengthFor(s.toString())
+                }
+
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                }
+            })
+
+            val passwordPage = LinearLayout(context)
+            passwordPage.orientation = LinearLayout.VERTICAL
+            passwordPage.addView(passwordBox)
+            passwordPage.addView(passwordStrengthMeter)
+
+            frame.addView(passwordPage)
+        }
+
+        return v
+    }
+}
+
+class EndFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_container, container, false)
+        val frame = v.findViewById<FrameLayout>(R.id.frame)
+
+        val textView = TextView(context)
+        textView.text = "Goodbye"
+        textView.textSize = 50.0f
+        frame.addView(textView)
+
+        return v
     }
 }
--- app/src/main/java/ninja/segerback/interaktionsprogrammering/StepsLeft.kt
+++ app/src/main/java/ninja/segerback/interaktionsprogrammering/StepsLeft.kt
@@ -2,21 +2,13 @@ package ninja.segerback.interaktionsprogrammering
 
 import android.content.Context
 import android.graphics.Color
-import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
-import android.text.Editable
-import android.text.InputType
-import android.text.TextWatcher
 import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.*
-
-const val NUM_STEPS = 2
+import android.widget.Button
+import android.widget.LinearLayout
 
 class StepsLeft(context: Context, stepNames: List<String>, pager: ViewPager): LinearLayout(context) {
     private val buttons: List<Button>
@@ -25,10 +17,9 @@ class StepsLeft(context: Context, stepNames: List<String>, pager: ViewPager): Li
         gravity = Gravity.CENTER
         orientation = LinearLayout.HORIZONTAL
 
-        buttons = stepNames.mapIndexed { i, step ->
-            val step = stepNames[i]
+        buttons = stepNames.map {
             val button = Button(context)
-            button.text = step
+            button.text = it
             button.setBackgroundColor(Color.TRANSPARENT)
             addView(button)
             button
@@ -40,114 +31,22 @@ class StepsLeft(context: Context, stepNames: List<String>, pager: ViewPager): Li
                 highlightStep(i)
             }
         }
+
+        highlightStep(0)
     }
 
-    public fun highlightStep(i: Int) {
+    fun highlightStep(i: Int) {
         for (b in buttons) { b.setBackgroundColor(Color.TRANSPARENT) }
         buttons[i].setBackgroundColor(Color.rgb(200, 200, 255))
     }
 }
 
-class StepsAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+class StepsAdapter(fm: FragmentManager, private val steps: List<() -> Fragment>) : FragmentPagerAdapter(fm) {
     override fun getCount(): Int {
-        return 3
+        return steps.size
     }
 
     override fun getItem(position: Int): Fragment {
-        val frag = StepFragment()
-
-        val args = Bundle()
-        args.putInt("num", position)
-        frag.arguments = args
-
-        return frag
+        return steps[position]()
     }
-}
-
-class StepFragment : Fragment() {
-    private var num: Int = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        num = arguments?.getInt("num") ?: 0
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val v = inflater.inflate(R.layout.fragment_container, container, false)
-        val frame = v.findViewById<FrameLayout>(R.id.frame)
-
-        val c = context
-        when (num) {
-            0 -> {
-                val textView = TextView(context)
-                textView.text = "Hello and welcome"
-                textView.textSize = 30.0f
-                frame.addView(textView)
-            }
-            1 -> if (c != null) {
-                frame.addView(makePasswordPage(c))
-            }
-            2 -> {
-                val textView = TextView(context)
-                textView.text = "Goodbye"
-                textView.textSize = 50.0f
-                frame.addView(textView)
-            }
-        }
-        return v
-    }
-}
-
-fun makePasswordPage(context: Context): LinearLayout {
-    val passwordStrengthMeter = PasswordStrengthMeter(context) { password ->
-        var score = 0
-
-        // Check password length
-        if (password.length > 5) {
-            score++
-        }
-        if (password.length > 8) {
-            score++
-        }
-
-        // Check that the password contains both upper and lower case letters
-        if (password.toLowerCase() != password && password.toUpperCase() != password) {
-            score++
-        }
-
-        // Check that the password contains special characters
-        if (password.any { c -> !c.isLetter() }) {
-            score++
-        }
-
-        when (score) {
-            0 -> PasswordStrength.VeryWeak
-            1 -> PasswordStrength.Weak
-            2 -> PasswordStrength.Mediocre
-            3 -> PasswordStrength.Good
-            else -> PasswordStrength.Strong
-        }
-    }
-    passwordStrengthMeter.setVisualizer(TextPasswordVisualizer(context))
-
-    val passwordBox = EditText(context)
-    passwordBox.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-    passwordBox.addTextChangedListener(object: TextWatcher {
-        override fun afterTextChanged(s: Editable) {
-            passwordStrengthMeter.showStrengthFor(s.toString())
-        }
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-        }
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-        }
-    })
-
-    val passwordPage = LinearLayout(context)
-    passwordPage.orientation = LinearLayout.VERTICAL
-    passwordPage.addView(passwordBox)
-    passwordPage.addView(passwordStrengthMeter)
-
-    return passwordPage
 }
